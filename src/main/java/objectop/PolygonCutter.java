7161d24989207337a87c86ac8b2a15238dc2ac2a
Anhelina Kulkova <a.kulkova.ip21@kpi.ua>
2023-11-10 18:22:45 +0100

    PolygonCutter started implementation

---
+++ src/main/java/objectop/PolygonCutter.java
@@ -0,0 +1,14 @@
+package objectop;
+
+import model.Polygon;
+
+/**
+ * Represents the Shuterland-Hodgman algorithm for polygon cutter
+ */
+public class PolygonCutter {
+    public Polygon cut(Polygon cutiingPolygon, Polygon polygonToCut){
+        //TODO 
+        return null;
+    }
+
+}
